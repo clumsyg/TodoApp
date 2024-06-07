03729204fa5a80f7f47d2dcceee9463acf432c8e
clumsyg <rutobe740@gmail.com>
2024-06-07 09:19:11 +0900

    build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    // base: process.env.GITHUB_PAGES
-    //     ? "TodoApp"
-    //     : "./"
+    base: process.env.GITHUB_PAGES
+        ? "TodoApp"
+        : "./",
+    VITE_API_URL: "https://clumsyg.github.io/TodoApp/"
 })
